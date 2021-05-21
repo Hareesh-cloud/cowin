7349d2ebc898de013ab0447303e06c135fe5f785
deepak.mishra <deepak.mishra@rocketflyer.in>
2021-05-21 13:34:12 +0530

    changes'

--- src/main/java/com/hcl/cowin/HclCowinApplication.java
+++ src/main/java/com/hcl/cowin/HclCowinApplication.java
@@ -2,11 +2,12 @@ package com.hcl.cowin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class HclCowinApplication {
+public class HclCowinApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HclCowinApplication.class, args);
